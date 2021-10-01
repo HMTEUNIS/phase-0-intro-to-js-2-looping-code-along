7bb3cf30ebf3b3207f4c8de188fa56b382540dc5
Holly Teunis <+>
2021-10-01 13:53:13 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+function writeCards(nameList, event) {
+    let newArray = []
+    for (let i = 0; i < nameList.length; i++) {
+        let name = nameList[i]
+
+        newArray.push(
+            "Thank you, " + name + ", for the wonderful " + event + " gift!" 
+        );
+    }
+    console.log(newArray)
+    return newArray
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(i) {
+let countDown = i;
+while (i > -1) 
+  console.log(i--)
+}
+countDown(10)
+
